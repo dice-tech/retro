fab94db473791e7cee4a67c80cda50b66e3b553f
Antrepriz <lakshay@antrepriz.com>
2022-07-30 23:49:24 +0530

    Automated Commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './styles.module.css'
+import Theme from './Theme'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export {Theme};
